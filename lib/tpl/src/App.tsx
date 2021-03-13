b223cf39e11d99cc11351a749902b2d20cc84ffc
FY1010 <1114550440@qq.com>
2021-03-13 23:56:04 +0800

    fix tpl

--- lib/tpl/src/App.tsx
+++ lib/tpl/src/App.tsx
@@ -1,20 +1,57 @@
 import { ref, FC } from "vvix";
+import "./style.scss";
+
+interface Props {
+  title: string;
+  content: string;
+}
+
+const data = [
+  {
+    id: 0,
+    title: "Better Call Saul",
+    content:
+      "The best TV Series EVER! Though it's a little bit " +
+      " slow tempo at first. You mustn't miss it.",
+  },
+  {
+    id: 1,
+    title: "Breaking Bad",
+    content:
+      "The second best series, it's the best until I meet 《Better Call Saul》",
+  },
+  {
+    id: 2,
+    title: "The Walking Dead",
+    content: "It's interesting at first, however...",
+  },
+];
+
+const Msg: FC<Props> = ({ title, content }) => () => (
+  <div class="msg">
+    <h1>{title}</h1>
+    <p>{content}</p>
+  </div>
+);
 
 const App: FC = () => {
+  const info = ref(data);
   const count = ref(0);
-
+  const click = () => {
+    count.value++;
+  };
   return () => (
-    <div
-      onClick={() => {
-        count.value++;
-      }}
-    >
-      <h1>hello vvix</h1>
-      hello world {count.value}
-      very welcome PR and Star
-      <br></br>
-      <a href="https://github.com/JSerFeng/vvix">github</a>
-    </div>
+    <>
+      <h1 class="title" onClick={click}>
+        vvix
+        <div>click count: {count.value}</div>
+      </h1>
+      <div>
+        {info.value.map((msg) => (
+          <Msg key={msg.id} title={msg.title} content={msg.content} />
+        ))}
+      </div>
+    </>
   );
 };
 export default App;
